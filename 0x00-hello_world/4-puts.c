c158e1830d9641ee200d0b16c489c27fd057c351
Ricarhincapie <hincapiet.ricardo@gmail.com>
2020-02-06 01:57:56 +0000

    Add 4-puts v3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#include <string.h>
-
-/*Prints a string with puts function*/
-
+/**
+  * main - prints a string
+  * Return:0
+  */
 int main(void)
 {
-	char string[40];
-
-	strcpy(str, "\"Programming is like building a multilingual puzzle");
-	puts(string);
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
