49f491c0fc3dec2a50d1f57cd3fbc758f9c26a5e
Ricarhincapie <hincapiet.ricardo@gmail.com>
2020-02-06 23:10:50 +0000

    Add 2-print v1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - It prints the alphabet
+ * Return:0
+*/
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+		putchar(n);
+	putchar('\n');
+
+return (0);
+}
