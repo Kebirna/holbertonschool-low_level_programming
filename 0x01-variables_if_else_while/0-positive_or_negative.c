33f759f64f47328c7dfd83b8f057b2bdd24e0a25
Ricarhincapie <hincapiet.ricardo@gmail.com>
2020-02-06 19:54:42 +0000

    Add 0-positive v3

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * Random number call - It says wheter a random number is positive, negative or zero
- * Return = 0
+ * main - It says whether if # is positive, negative or zero
+ * return = 0
 */
-
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
