e1fde62d604534d31e904d81718113d96a9cb154
Ricarhincapie <hincapiet.ricardo@gmail.com>
2020-02-07 03:17:50 +0000

    Add 8-print v2

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+  * main - It prints 0-9 int
+  * Return:0
+  */
+int main(void)
+{
+	int i;
+	char l;
+
+	i = '0';
+	l = 'a';
+
+	while (i <= '9')
+	{
+	putchar(i);
+	i++;
+	}
+	
+	while (l <= 'f')
+	{
+	putchar(l);
+	l++;
+	}
+	
+	printf("\n");
+return (0);
+}
