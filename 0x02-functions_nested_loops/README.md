6f99d9497423ad03b6ad66c189de4df51d264db3
Ricarhincapie <hincapiet.ricardo@gmail.com>
2020-02-10 23:28:08 +0000

    Add directory and 0 excercise

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Readme.md for our Functions and nested loopsReadme.md for our Functions and nested loops
---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,13 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+
+/*
+    * Structs, enums and unions definitions
+     * Typedefs
+      * Function prototypes
+       */
+
+int _putchar(char c);
+
+
+#endif /* _SAMPLE_HEADER_H_ */
