74f1795d4e2f2617fa7a704bb0b8f042979f6197
Ricarhincapie <hincapiet.ricardo@gmail.com>
2020-02-11 22:39:35 +0000

    Add #5 to #8 task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * abs - prints the absolute value of an int
+ *
+ *Return: 0;
+ */
+
+int _abs(int r)
+{
+	int absolute;
+
+	absolute = abs(r);
+
+	return(absolute);
+	_putchar('\n');	
+}	
