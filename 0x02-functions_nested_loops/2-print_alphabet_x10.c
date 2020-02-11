7bd0a195f2ba78cb44503359964707f5c5e52edb
Ricardo  Hincapie <1516@holbertonschool.com>
2020-02-11 03:38:02 +0000

    Add #1 n #2 tasks

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet x10 - uses the print_alphabet func
+ *
+ *Return: nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	int rank; 
+	int letter;
+
+	rank = 0;
+
+	while (rank < 10)
+	{
+		letter = 97;
+		while (letter < 123)
+		{
+			_putchar(letter);
+			letter++;
+		}
+		letter = 97;
+		_putchar('\n');
+		rank++;
+	}
+}
