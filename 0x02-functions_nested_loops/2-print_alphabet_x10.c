2b4bf525098c69f30ab56544f47607be2a5d445d
Ricarhincapie <hincapiet.ricardo@gmail.com>
2020-02-12 22:24:36 +0000

    Add #1 to # 3 V2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,14 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- * print_alphabet x10 - uses the print_alphabet func
+ * print_alphabet_x10 - uses the print_alphabet func
  *
- *Return: nothing
+ * Return: nothing
  */
 
 void print_alphabet_x10(void)
 {
-	int rank; 
+	int rank;
 	int letter;
 
 	rank = 0;
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,14 @@
 #include "holberton.h"
-#include <stdio.h>
-#include <ctype.h>
-
 /**
  * _islower - checks if c is lowercase
  *
- *Return: 1 if positive, else 0
+ * Return: 1 if positive, else 0
  */
 
 int _islower(int c)
 {
-	if (islower(c) > 0)
-		return(1);
+	if (c >= 'a' && c <= 'z')
+		return (1);
 	else
-		return(0);
-}		
+		return (0);
+}
