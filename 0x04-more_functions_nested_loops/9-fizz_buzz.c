7502bfc1962e450d7eb3a2d828732d1560c4e66f
Ricarhincapie <hincapiet.ricardo@gmail.com>
2020-02-14 04:50:20 +0000

    Add #9 v1

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * fizz_buzz - prints fizz and buzz according to multiples of 5 and 3
+ *
+ */
+int main(void)
+{
+	int x;
+	int Vfiz;
+	int Vbuz;
+
+	for (x = 1; x < 101; x++)
+{
+	Vfiz = x % 3;
+	Vbuz = x % 5;
+
+
+	if (Vfiz == 0)
+	{
+		printf("Fizz ");
+	}
+	else if (Vbuz == 0 && x < 100)
+	{
+		printf("Buzz ");
+	}
+	else if (Vfiz == 0 && Vbuz == 0)
+	{
+		printf("FizzBuzz ");
+	}
+	else if (x == 100)
+	{
+		printf("Buzz");
+	}
+	else
+	{
+		printf("%d ", x);
+	}
+}
+putchar('\n');
+return (0);
+}
