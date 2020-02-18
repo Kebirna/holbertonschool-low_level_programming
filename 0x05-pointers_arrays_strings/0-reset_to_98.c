7784a217222c49fcc6d65575d9e758fe7750df3c
Ricarhincapie <hincapiet.ricardo@gmail.com>
2020-02-18 19:02:14 +0000

    Add #0 task v1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * void reset_to_98(int *n) - checks if c is lowercase
+ *
+ *Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}		
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,23 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+
+/**
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void reset_to_98(int *n);
+
+#endif /* _SAMPLE_HEADER_H_ */
