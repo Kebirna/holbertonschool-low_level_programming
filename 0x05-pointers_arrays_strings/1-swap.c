8fa07c7cb498edd383818875d63f6ad7e460a5b7
Ricarhincapie <hincapiet.ricardo@gmail.com>
2020-02-18 19:21:15 +0000

    Add task #1 v1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * swap_int - checks if c is lowercase
+ * @a: the ponter to be used
+ * @b: the ponter to be used
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
