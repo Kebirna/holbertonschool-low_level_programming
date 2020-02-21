d9ebe625d8e9f2a529d98694d317beb0fbcb4b15
Ricarhincapie <hincapiet.ricardo@gmail.com>
2020-02-21 04:32:12 +0000

    Add task #6 v2

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * *string_toupper - changes lowercase to uppercase
+ * string_toupper - changes lowercase to uppercase
  * @a: pointer to the string to be changed
  * Return: the character changed
  *
@@ -10,11 +10,15 @@
 char *string_toupper(char *a)
 {
 	int i;
+	i = 0;
 
-	while (a[i] != '\0' && a[i] > 'a' && a[i] < 'z')
+	while (a[i] != '\0' && a[i] >= 'a' && a[i] <= 'z')
 	{
+		while (a[i] >= 'a' && a[i] <= 'z')
+		{
 		a[i] = a[i] - 32;
-		i++;
+		}
 	}
+	i++;
 	return(a);
 }
