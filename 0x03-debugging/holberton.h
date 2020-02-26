254d563d064ddfa7e48f16c88954434e642f529f
Ricardo  Hincapie <1516@holbertonschool.com>
2020-02-26 03:54:09 +0000

    Add directory and all tasks

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,19 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+
+/*
+    * Structs, enums and unions definitions
+     * Typedefs
+      * Function prototypes
+       */
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif /* HOLBERTON_H */
