3824f81ffba4411a70353e97459bcd5ea85e4c22
Ricarhincapie <hincapiet.ricardo@gmail.com>
2020-03-04 02:42:22 +0000

    Add task #2 v4

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,56 +3,41 @@
 #include <stdlib.h>
 
 /**
- * *_strdup - copies an string into a new memory allocation
- * @str: the pointer to the string to copy
- *
+ * *str_concat - concatenates two strings
+ * @s1: the pointer to the string to copy
+ * @s2: amo a mi mama
  * Return: Pointer to the new string
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	int i;
-	int j;
-	int k;
-	int size;
-	int l;
+	int i, j, k, l, size;
 	char *ptr;
 
-	i = 0;
-	j = 0;
-	k = 0;
-
+	i = j = k = l = 0;
 	if (s1 == NULL)
 	{
 		s1 = malloc(1 * (sizeof(char)));
 		s1[0] = '\0';
 	}
-
 	if (s2 == NULL)
 	{
 		s2 = malloc(1 * (sizeof(char)));
 		s2[0] = '\0';
 	}
-
 	while (s1[i] != '\0')
 	{
 		i++;
 	}
-
 	while (s2[j] != '\0')
 	{
 		j++;
 	}
 	size = j + i;
-
 	ptr = malloc(size * (sizeof(char)));
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
-
 	l = 0;
-
 	while (k < i)
 	{
 		ptr[k] = s1[k];
