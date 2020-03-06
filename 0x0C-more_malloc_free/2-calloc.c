0c5d7fb5c0e64a49535a6b09002c6b59ea14cf70
Ricardo  Hincapie <1516@holbertonschool.com>
2020-03-06 01:37:01 +0000

    Add task #2 v1

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - functions that allocates memory for an array & fills it with zero
+ * @nmemb: # of units of memory to be allocated
+ * @size: size of each unit to be allocated
+ *
+ * Return: Pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	int total;
+	int count;
+
+	if (nmemb < 1 || size < 1)
+		return (NULL);
+	total = nmemb * size;
+	ptr = malloc(total);
+	if (ptr == NULL)
+		return (NULL);
+
+	count = 0;
+	while (count < total)
+	{
+		ptr[count] = 0;
+		count++;
+	}
+	return (ptr);
+}
