4f41080e8ed732c844e159347112c22c569c3ee6
Ricardo  Hincapie <1516@holbertonschool.com>
2020-03-06 02:39:37 +0000

    Add task #3 v1

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *array_range - create an array with the # between 2 points
+ * @min: starting point (included in array)
+ * @max: finishing point (included in array)
+ *
+ * Return: Pointer to the allocated memory
+ */
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int n;
+	int range;
+
+	range = (max - min) + 1;/* 10 - 0 = 10*/
+	ptr = malloc(sizeof(int) * range);
+
+	n = 0;
+	while (min <= max)
+	{
+		ptr[n] = min;
+		n++;
+		min++;
+	}
+	return (ptr);
+}
