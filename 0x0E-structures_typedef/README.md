72becd0a40a4c19800308d04b4eea97dc20a0944
Ricarhincapie <hincapiet.ricardo@gmail.com>
2020-03-09 23:05:04 +0000

    Add Readme & task #0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,7 @@
+# holbertonschool-low_level_programming
+
+STRUCTURES AND TYPEDEF 
+
+Here are the result of the learning process about structures and typedef
+
+Work by: @RicarHincapie
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Keeps basic information about dogs
+ * @name: Name of the dog
+ * @age: the dog's age
+ * @owner: the dog's owner
+ *
+ * Description: Simple dog information
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
