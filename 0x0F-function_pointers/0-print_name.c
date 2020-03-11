42aa3a4941b2b22f3e60ecba9194ef1afbf3c862
Ricarhincapie <hincapiet.ricardo@gmail.com>
2020-03-11 19:21:42 +0000

    Directory creation, task #0 v1, Readme and header file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - takes an input string and directs it to a named function
+ * @name: input name
+ * @f: pointer to the function inputed
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if(name && f)
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,12 @@
+#ifndef _FUNCTION_POINTERS_H
+#define _FUNCTION_POINTERS_H
+
+/**
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+void print_name(char *name, void (*f)(char *));
+
+#endif /* _FUNCTION_POINTERS_H_ */
