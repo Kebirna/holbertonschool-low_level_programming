55ff08526bddba2af592d4caa89e922d39ee4c4b
Ricarhincapie <hincapiet.ricardo@gmail.com>
2020-03-11 20:12:35 +0000

    Add task #1 v1, updated header

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_iterator - loops in an array and sends each element to function
+ * @array: name of the array to be proccessed
+ * @size: size of the array
+ * @action: pointer to the function to be activated
+ * Return: Nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int counter;
+
+	if (array && size && action)
+	{
+		counter = 1;
+		while (counter <= size)
+		{
+			(*action) (array[counter]);
+			counter++;
+		}
+	}
+	else
+		return;
+}
