88412011d78d12febb7426368d323f4d71f68024
Ricardo Hincapie <1516@holbertonschool.com>
2020-03-20 03:50:39 +0000

    Add task #2 v1 & header file

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+/**
+ * *add_node - Adds a new node at the begining of the struct
+ * @**head: pointer to the header of struct
+ * @*srt: data to be included into the node
+ * Return: pointer to the new head of struct
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newnode;
+	int lenght;
+
+	newnode = malloc(sizeof(list_t));
+	
+	if(newnode == NULL)
+		return (NULL);
+	else
+	{
+		lenght = _strlen(str);
+		newnode->str = strdup(str);
+		newnode->len = lenght;
+		newnode->next = *head;
+	
+	*head = newnode;
+	return (newnode);
+	}
+}
+
+/**
+ * _strlen - returns length of a string
+ * @s: pointer to string
+ *
+ * Return: String lenght
+ */
+int _strlen(const char *s)
+{
+        int i = 0;
+
+        while (*(s + i) != '\0')
+        {
+                i++;
+        }
+
+return (i);
+}
