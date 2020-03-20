a09cfed84b2cb0c40ae8cbdd9ba33a4e44aab0f6
Ricardo Hincapie <1516@holbertonschool.com>
2020-03-20 04:55:30 +0000

    Add task 100 v1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * print_before - Prints befor main function
+ *
+ *
+ * Return: None
+ */
+
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
