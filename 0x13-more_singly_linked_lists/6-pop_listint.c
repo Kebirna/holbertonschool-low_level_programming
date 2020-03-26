c92421c91e83169342e77055f75379cbdba1daaf
Ricardo Hincapie <1516@holbertonschool.com>
2020-03-26 00:12:08 +0000

    Add task #6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,13 +13,15 @@ int pop_listint(listint_t **head)
 	listint_t *for_free;
 	int data;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (0);
+
 	temporal = *head;
 	for_free = *head;
 	data = temporal->n;/*is tmp already in the node?*/
 	temporal = temporal->next;
 	*head = temporal;
 	free(for_free);
+
 	return (data);
 }
