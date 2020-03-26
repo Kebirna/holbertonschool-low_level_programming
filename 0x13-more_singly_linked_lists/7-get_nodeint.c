30f2bc36e7d15a965c7c75db0fbd6a3d4be2a587
Ricardo Hincapie <1516@holbertonschool.com>
2020-03-26 00:59:38 +0000

    Add task #7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,11 +13,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	counter = 0;
 
+	if (!head)
+		return (NULL);
+
 	while (counter < index)
 	{
-		head = head->next;
-		if (head->next == NULL)
+		if (head == NULL)
 			return (NULL);
+		head = head->next;
 		counter++;
 	}
 return (head);
