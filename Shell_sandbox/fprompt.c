cb06f6121050194b83818d489422d1f0600e9302
Ricardo Hincapie <1516@holbertonschool.com>
2020-04-06 23:57:19 +0000

    fprompt

--- Shell_sandbox/fprompt.c
+++ Shell_sandbox/fprompt.c
@@ -3,8 +3,31 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 /*#include "holberton.h"*/
 
+char **parsing(char *buffer, int characters)
+{
+	char **token_necklace = malloc(sizeof(char *) * characters);
+	char *token;
+	char *delimiter = " :'\n''\t'";  //Review this behaivor 
+	int counter = 0;
+
+	/*spaces = count_spaces(buffer);*/
+	/***token_necklace = malloc(sizeof(char *) * characters);*/
+	if(token_necklace == NULL)
+		printf("Error");
+		/*return(NULL);*/
+	token = strtok(buffer, delimiter);
+	while(token != NULL)
+	{
+		token_necklace[counter] = strdup(token);
+		token = strtok(NULL, delimiter);
+		counter++;
+	}
+	token_necklace[counter] = token;
+	return(token_necklace);
+}
 
 int main(int ac, char **av, char **env)
 {
@@ -13,9 +36,8 @@ int main(int ac, char **av, char **env)
 	size_t characters;
 	int counter;
 	char *token;
-	char delimiter[2] = " ";
 	char **args;  // args[0]= /bin/ls args[1]="-l" 
-
+	int status;
 	
 
 	while (1)
@@ -28,11 +50,10 @@ int main(int ac, char **av, char **env)
 			characters = getline(&buffer, &bufsiz, stdin);
 			args = parsing(buffer, characters);
 			execve(args[0], args, NULL);
-			
 		}
 		else
 		{
-			wait(NULL);
+			wait(&status);
 		}	
 	}
 	
