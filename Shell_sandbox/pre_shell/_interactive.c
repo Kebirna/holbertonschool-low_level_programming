8d2acace659830c07adf375014ecb37e16da96e2
Ricardo Hincapie <1516@holbertonschool.com>
2020-04-10 22:13:31 +0000

    Add interactive function

---
+++ Shell_sandbox/pre_shell/_interactive.c
@@ -0,0 +1,27 @@
+#include "hsh.h"
+
+int _interactive(char **av __attribute__((unused)))
+{
+	char *buffer = NULL;
+	size_t bufsiz;
+	char **args = NULL;
+	int size;
+
+	while(1)
+	{
+		write(STDOUT_FILENO, "$ ", 2);
+		if (getline(&buffer, &bufsiz, stdin) != EOF)
+		{
+			size = necklace_pearls(buffer);
+			args = parsing(buffer, size);
+			execo(args);
+		}
+		else
+		{
+			write(STDOUT_FILENO, "\n", 1);
+			break;
+		}
+		
+	}
+return(0);
+}
