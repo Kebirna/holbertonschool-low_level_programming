349407189270ddd17b478df0f33cf1badf103a19
Ricardo Hincapie <1516@holbertonschool.com>
2020-05-04 15:53:07 +0000

    Add task #5 corrected

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,8 +10,17 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *tmp = head;
+	dlistint_t *tmp2 = head;
 	unsigned int counter = 0;
+	unsigned int counter2 = 0;
 
+	while (tmp2->next != '\0')
+	{
+		tmp2 = tmp2->next;
+		counter2++;
+	}
+	if (index > counter2)
+		return (NULL);
 	for (; counter < index; counter++)
 		tmp = tmp->next;
 	return (tmp);
