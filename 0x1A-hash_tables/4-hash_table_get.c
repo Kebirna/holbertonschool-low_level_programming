368cd0ea32f89169c5aeda52575c30586cecbd66
Ricardo Hincapie <1516@holbertonschool.com>
2020-06-01 02:12:30 +0000

    Fix collision case task #4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,10 +14,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (ht == NULL || key == NULL)
 		return (0);
 
-	index = key_index((const unsigned char *)key, SIZE);
-	if (ht->array[index] != NULL)
-		return (ht->array[index]->value);
-	else
-		return (0);
-
+	index = key_index((const unsigned char *)key, ht->size);
+	while (ht->array[index] != NULL)
+	{
+		if (strcmp(key, ht->array[index]->key) == 0)
+			return (ht->array[index]->value);
+		ht->array[index] = ht->array[index]->next;
+	}
+	return (0);
 }
