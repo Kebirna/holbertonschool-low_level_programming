24ea3fecf3c2b0ce5925a35df96528d3421903f1
Ricardo Hincapie <1516@holbertonschool.com>
2020-06-01 02:34:11 +0000

    Fix collision case task #5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,23 +9,39 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-	int i = 0;
+	unsigned int i = 0;
 	int flag = 0;
 
 	if (ht == NULL)
 		return;
 	printf("{");
-	while (i < SIZE)
+	while (i < ht->size)
 	{
 		if (ht->array[i] != NULL && flag == 0)
 		{
-			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			if (ht->array[i]->next == NULL)
+				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			else
+			{
+				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+				ht->array[i] = ht->array[i]->next;
+				printf(", '%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			}
 			flag++;
 			i++;
 			continue;
 		}
 		if (ht->array[i] != NULL)
-			printf(", '%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+		{
+			if (ht->array[i]->next == NULL)
+				printf(", '%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			else
+			{
+				printf(", '%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+				ht->array[i] = ht->array[i]->next;
+				printf(", '%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			}
+		}
 		i++;
 	}
 	printf("}\n");
