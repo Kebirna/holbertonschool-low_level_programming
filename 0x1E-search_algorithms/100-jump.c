d1adbfbc950a9e75358e8866d81885a0361c3f6f
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-08-28 16:54:45 +0000

    Add task #100 correction

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -18,7 +18,7 @@ int jump_search(int *array, size_t size, int value)
 
 	while (back < size)
 	{
-		if (array[front] >= value || back == size - 1)
+		if (array[front] >= value || back == size)
 		{
 			back = front - jump;
 			printf("Value found between indexes [%lu] and [%lu]\n", back, front);
